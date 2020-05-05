80cd5521737a8ec48e9a5fc79f0e8903ff0f803d
papagianEva <evapapag2000@gmail.com>
2020-05-05 13:49:53 +0300

    Connection to db sqlite

--- src/Users.java
+++ src/Users.java
@@ -1,4 +1,16 @@
-
+import java.sql.*;
 public class Users extends ListFromDB {
-
+	
+	public Users(){
+		Connection c = null;
+		try {
+			
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+			System.out.println("SQLite DB connected");
+			
+		}catch(Exception e){
+			System.out.println(e);
+		}
+	}
 }
