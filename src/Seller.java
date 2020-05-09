7bb798bd09208537f8d86ddb37dba53443601f5e
dai19303 <dai19303@uom.edu.gr>
2020-05-09 16:57:32 +0300

    Add Constructor and Getters

--- src/Seller.java
+++ src/Seller.java
@@ -1,4 +1,20 @@
 
 public class Seller extends User {
+	
+	private CompanyProducts products;
+	private Buyers buyers;
+	
+	//Constructor
+	public Seller(CompanyProducts products, Buyers buyers) {
+		this.products = products;
+		this.buyers = buyers;
+	}
 
+	public CompanyProducts getProducts() {
+		return products;
+	}
+
+	public Buyers getBuyers() {
+		return buyers;
+	}
 }
