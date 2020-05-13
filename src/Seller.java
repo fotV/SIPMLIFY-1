25be7fcc1539abecb4d7ee7e56d198806e7ce7e9
dai19303 <dai19303@uom.edu.gr>
2020-05-13 20:36:24 +0300

    Methods addProduct() & editProduct() : S

--- src/Seller.java
+++ src/Seller.java
@@ -28,7 +28,15 @@ public class Seller extends User {
 		}
 	}
 	
+	/* Method addProduct() : adds the parameter to the product list */
+	public void addProduct(CompanyProducts product) {
+		products.add(product);
+	}
 	
+	/*Method editProduct() : adds the cp in the specific index position of list products */
+	public void editProduct(CompanyProducts cp, int index) {
+		products.add(index, cp);
+	}
 	
 	public CompanyProducts getProducts() {
 		return products;
