a715e0442776a5eedc7390d7db0f8e87cecbf23e
papagianEva <evapapag2000@gmail.com>
2020-05-22 15:17:37 +0300

    no message

--- src/Stockkeeper.java
+++ src/Stockkeeper.java
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 package src;
 
-=======
->>>>>>> d84165162798e8ae4759bbfd617c789eea67382b
 public class Stockkeeper extends User {
 
 	public Stockkeeper(String firstName, String surName, String password, String telephone, String aMA, String id,String company) {
