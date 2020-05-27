80f5f4a5e9b56ad1ca25e73567a6357e181aabe2
ChristinaManoli <dai19181@uom.edu.gr>
2020-05-27 12:22:19 +0300

    Fixed errors and made some changes.

--- src/PresentationForm.java
+++ src/PresentationForm.java
@@ -1,4 +1,169 @@
+import java.awt.BorderLayout;
+import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import java.awt.SystemColor;
+import java.awt.Color;
 
-public class PresentationForm {
 
+public class PresentationForm extends JFrame{
+	
+	private JPanel panel;
+	private JTable table = new JTable();
+	private JScrollPane scroll;
+	private int n;
+	
+	/*
+	 * The following constructor creates the gui for the Presentation Form which is used to present very briefly to the user
+	 * a list of data in order for them to choose one to open and see it in more detail.
+	 */
+	public PresentationForm(ArrayList <Object> objectList)
+	{
+		this.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Christina\\Desktop\\3\\Organizedorders\\window logo.png"));
+		String[][] data = new String[objectList.size()][3];
+		panel = new JPanel();
+		panel.setBackground(new Color(240, 255, 255));
+		panel.setBounds(100, 100, 584, 534);
+		
+		scroll = new JScrollPane();
+		panel.add(scroll, BorderLayout.CENTER);
+
+
+		if (objectList.get(0) instanceof Order)    //here we check if the objects in the list we have are of type Order
+		{
+			n=1;
+			for (int i=0; i<objectList.size(); i++)
+			{
+				Order temp = (Order) objectList.get(i);
+				data[i][0]=temp.getProductName();
+				data[i][1]=temp.getOrderId();
+				data[i][2]=temp.getStatus()+"";
+			}
+			table.setModel(new DefaultTableModel(data, new String[] {"Product Name", "Order ID", "Status"}));
+		}
+		else if (objectList.get(0) instanceof CompanyProduct) //here we check if the objects in the list we have are of type CompanyProduct
+		{
+			n=2;
+			for (int i=0; i<objectList.size(); i++)
+			{
+				CompanyProduct temp = (CompanyProduct) objectList.get(i);
+				data[i][0]=temp.getName();
+				data[i][1]=temp.getId();
+				data[i][2]=temp.getStockamount()+"";
+			}
+			table.setModel(new DefaultTableModel(data, new String[] {"Product Name", "Product Id", "StockAmount"}));
+		}
+		else if (objectList.get(0) instanceof SupplierProduct) //here we check if the objects in the list we have are of type SupplierProduct
+		{
+			n=3;
+			for (int i=0; i<objectList.size(); i++)
+			{
+				SupplierProduct temp = (SupplierProduct) objectList.get(i);
+				data[i][0]=temp.getName();
+				data[i][1]=temp.getId();
+				data[i][2]=temp.getStockAmount()+"";
+			}
+			table.setModel(new DefaultTableModel(data, new String[] {"Product Name", "Product Id", "StockAmount"}));
+		}
+		else if (objectList.get(0) instanceof Supplier) //here we check if the objects in the list we have are of type Supplier
+		{
+			n=4;
+			for (int i=0; i<objectList.size(); i++)
+			{
+				Supplier temp = (Supplier) objectList.get(i);
+				data[i][0]=temp.getName();
+				data[i][1]=temp.getId();
+				data[i][2]=temp.getAFM();
+			}
+			table.setModel(new DefaultTableModel(data, new String[] {"Supplier Name", "Supplier Id", "Supplier AFM"}));
+		}
+		else if (objectList.get(0) instanceof Buyer) //here we check if the objects in the list we have are of type Buyer
+		{
+			n=5;
+			for (int i=0; i<objectList.size(); i++)
+			{
+				Buyer temp = (Buyer) objectList.get(i);
+				data[i][0]=temp.getName();
+				data[i][1]=temp.getId();
+				data[i][2]=temp.getAFM();
+			}
+			table.setModel(new DefaultTableModel(data, new String[] {"Buyer Name", "Buyer Id", "Buyer AFM"}));
+		}
+		table.setToolTipText("");
+		table.setForeground(Color.BLACK);
+		table.setBackground(new Color(176, 196, 222));
+		table.setFillsViewportHeight(true);
+		table.setAutoCreateRowSorter(true);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setSelectionBackground(Color.ORANGE);
+
+		scroll.setViewportView(table);
+		
+		
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				int index = table.getSelectedRow();
+		        
+		        TableModel model = table.getModel();
+		        
+		        String SelectedId = model.getValueAt(index, 1).toString();
+		        if(n==1)
+		        {
+			        for(Object o : objectList)
+					{
+			        	Order temp = (Order) o;
+						if(temp.getOrderId().equals(SelectedId)) new ShowOrder(temp);
+					}
+		        }
+		        else if (n==2)
+		        {
+		        	for(Object o : objectList)
+					{
+			        	CompanyProduct temp = (CompanyProduct) o;
+						if(temp.getId().equals(SelectedId)) new ShowProduct(temp);
+					}
+		        }
+		        else if (n==3)
+		        {
+		        	for(Object o : objectList)
+					{
+		        		SupplierProduct temp = (SupplierProduct) o;
+						if(temp.getId().equals(SelectedId)) new ShowProduct(temp);
+					}
+		        }
+		        else if(n==4)
+		        {
+		        	for(Object o : objectList)
+					{
+		        		Supplier temp = (Supplier) o;
+						if(temp.getId().equals(SelectedId)) new ShowBuyerSeller(temp);
+					}
+		        }
+		        else if (n==5)
+		        {
+		        	for(Object o : objectList)
+					{
+		        		Buyer temp = (Buyer) o;
+						if(temp.getId().equals(SelectedId)) new ShowBuyerSeller(temp);
+					}
+		        }
+			}
+		});
+		
+		this.setContentPane(panel);
+	
+		
+		this.setVisible(true);
+		this.setTitle("PresentationForm");
+		this.setBounds(100, 100, 511, 461);
+		this.setResizable(false);
+		//this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+	}
+	                    
+	
 }
--- src/StockkeeperForm.java
+++ src/StockkeeperForm.java
@@ -1,4 +1,157 @@
+import java.awt.BorderLayout;
 
-public class StockkeeperForm {
 
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import javax.swing.border.EtchedBorder;
+import java.awt.SystemColor;
+import javax.swing.UIManager;
+import javax.swing.JSeparator;
+
+public class StockkeeperForm extends JFrame{
+   
+	private JPanel panel;
+	private JButton btnSearchForOrder;
+	private JButton Refreshbutton;
+	private JLabel lblName;
+	private JLabel lblSurname;
+	private JLabel lblTelephone;
+	private JLabel lblAFM ;
+	private JLabel lblID;
+	private JLabel resultName;
+	private JLabel resultSurname;
+	private JLabel resultTelephone;
+	private JLabel resultAFM;
+	private JLabel resultID;
+
+	/*
+	 * The following constructor creates the gui for the Stock Keeper Form (home page)
+	 */
+	public StockkeeperForm(Stockkeeper stk)
+	{
+		panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBackground(new Color(176, 196, 222));
+		panel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+
+
+		btnSearchForOrder = new JButton("Search For Order");
+		btnSearchForOrder.setToolTipText("Search for Order");
+		btnSearchForOrder.setBackground(new Color(255, 255, 224));
+		btnSearchForOrder.setFont(new Font("Futura Bk BT", Font.PLAIN, 24));
+		btnSearchForOrder.setBounds(10, 448, 243, 79);
+		panel.add(btnSearchForOrder);
+	
+		Refreshbutton = new JButton();
+		Refreshbutton.setToolTipText("Refresh your data");
+		Refreshbutton.setBackground(new Color(255, 255, 224));
+		Refreshbutton.setIcon(new ImageIcon("C:\\Users\\Christina\\Desktop\\3\\Organizedorders\\refresh.png"));
+		Refreshbutton.setForeground(Color.WHITE);
+		Refreshbutton.setBounds(647, 448, 86, 85);
+		panel.add(Refreshbutton);
+		
+		lblName = new JLabel("First Name :");
+		lblName.setFont(new Font("Futura Bk BT", Font.PLAIN, 20));
+		lblName.setBounds(30, 46, 116, 32);
+		panel.add(lblName);
+		
+		lblSurname = new JLabel("SurName :");
+		lblSurname.setFont(new Font("Futura Bk BT", Font.PLAIN, 20));
+		lblSurname.setBounds(30, 110, 116, 32);
+		panel.add(lblSurname);
+		
+		lblTelephone = new JLabel("Telephone :");
+		lblTelephone.setFont(new Font("Futura Bk BT", Font.PLAIN, 20));
+		lblTelephone.setBounds(30, 180, 116, 32);
+		panel.add(lblTelephone);
+		
+		lblAFM = new JLabel("AFM :");
+		lblAFM.setFont(new Font("Futura Bk BT", Font.PLAIN, 20));
+		lblAFM.setBounds(30, 246, 116, 32);
+		panel.add(lblAFM);
+		
+		lblID = new JLabel("ID :");
+		lblID.setFont(new Font("Futura Bk BT", Font.PLAIN, 20));
+		lblID.setBounds(30, 308, 116, 32);
+		panel.add(lblID);
+		
+		
+		resultName = new JLabel(stk.firstName);
+		resultName.setFont(new Font("Futura Bk BT", Font.PLAIN, 20));
+		resultName.setBounds(232, 49, 226, 26);
+		panel.add(resultName);
+		
+		resultSurname = new JLabel(stk.surName);
+		resultSurname.setFont(new Font("Futura Bk BT", Font.PLAIN, 20));
+		resultSurname.setBounds(232, 110, 226, 26);
+		panel.add(resultSurname);
+		
+		resultTelephone = new JLabel(stk.telephone);
+		resultTelephone.setFont(new Font("Futura Bk BT", Font.PLAIN, 20));
+		resultTelephone.setBounds(232, 183, 226, 26);
+		panel.add(resultTelephone);
+		
+		resultAFM = new JLabel(stk.AFM);
+		resultAFM.setBackground(new Color(230, 230, 250));
+		resultAFM.setFont(new Font("Futura Bk BT", Font.PLAIN, 20));
+		resultAFM.setBounds(232, 249, 226, 26);
+		panel.add(resultAFM);
+		
+		resultID = new JLabel(stk.id);
+		resultID.setBackground(new Color(255, 250, 240));
+		resultID.setFont(new Font("Futura Bk BT", Font.PLAIN, 20));
+		resultID.setBounds(232, 311, 226, 26);
+		panel.add(resultID);
+		
+		
+		btnSearchForOrder.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent arg0)
+			{
+					new SearchForm(1, stk);
+			}	
+		});
+
+		Refreshbutton.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent arg0)
+			{
+				stk.refresh();
+			}	
+		});
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(27, 89, 692, 2);
+		panel.add(separator);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(30, 156, 692, 2);
+		panel.add(separator_1);
+		
+		JSeparator separator_2 = new JSeparator();
+		separator_2.setBounds(30, 223, 692, 2);
+		panel.add(separator_2);
+		
+		JSeparator separator_3 = new JSeparator();
+		separator_3.setBounds(30, 289, 692, 2);
+		panel.add(separator_3);
+		
+		
+		this.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Christina\\Desktop\\3\\Organizedorders\\window logo.png"));
+		this.setVisible(true);
+		this.getContentPane().add(panel, BorderLayout.CENTER);
+		this.setResizable(false);
+		this.setTitle("HomePage");
+		this.setBounds(100, 100, 759, 583);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
