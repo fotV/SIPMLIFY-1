673eafd6531b476844a678b89458352574f14d49
vassilikiF <fotiadouvassiliki@gmail.com>
2020-05-27 15:02:23 +0300

    buyer form,selection gui, statistics, create chart

--- src/BuyerForm.java
+++ src/BuyerForm.java
@@ -1,4 +1,237 @@
+	import java.awt.Color;
+	import java.awt.Font;
+	import java.awt.event.ActionEvent;
+	import java.awt.event.ActionListener;
+	import java.util.ArrayList;
 
-public class BuyerForm {
+	import javax.swing.JButton;
+	import javax.swing.JFrame;
+	import javax.swing.JLabel;
+	import javax.swing.JOptionPane;
+	import javax.swing.JPanel;
+	import javax.swing.JTextField;
+	import javax.swing.SwingConstants;
 
-}
+	
+public class BuyerForm extends JFrame
+{
+
+
+		private JPanel panel;
+		private JFrame frame;
+		private boolean errorFlag;//false if there is no error else true
+		private Seller seller;
+		
+		public BuyerForm(Seller seller)
+		{
+			this.seller = seller;
+		}
+		
+		public void addAClient()
+		{
+			//creation of gui
+			
+			frame = new JFrame();
+			panel=new JPanel();  
+			frame.getContentPane().setBackground(new Color(135, 206, 235));
+		
+			JLabel title = new JLabel("Client Form");
+			title.setFont(new Font("Tahoma", Font.BOLD, 15));
+			title.setBounds(194, 21, 176, 14);
+			frame.getContentPane().add(title);
+		
+			// name
+			
+			JLabel nameLabel = new JLabel("Name :");
+			nameLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+			nameLabel.setBounds(26, 86, 71, 14);
+			frame.getContentPane().add(nameLabel);
+			
+			JTextField  nameTXT = new JTextField();
+			nameTXT.setBounds(126, 85, 257, 20);
+			frame.getContentPane().add( nameTXT);
+			nameTXT.setColumns(10);
+			
+			//last name
+			
+			JLabel lastNameLabel = new JLabel("Last Name :");
+			lastNameLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+			lastNameLabel.setBounds(26, 136, 90, 14);
+			frame.getContentPane().add(lastNameLabel);
+			
+			JTextField lastNameTXT = new JTextField();
+			lastNameTXT.setColumns(10);
+			lastNameTXT.setBounds(126, 135, 257, 20);
+			frame.getContentPane().add(lastNameTXT);
+
+			//id
+			
+			JLabel idLabel = new JLabel("ID :");
+			idLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+			idLabel.setBounds(26, 186, 71, 14);
+			frame.getContentPane().add(idLabel);
+			
+			JTextField idTXT = new JTextField();
+			idTXT.setColumns(10);
+			idTXT.setBounds(126, 185, 257, 20);
+			frame.getContentPane().add(idTXT);
+			
+			JLabel idmaxCharacters = new JLabel("(6 characters)");
+			idmaxCharacters.setFont(new Font("Tahoma", Font.PLAIN, 10));
+			idmaxCharacters.setBounds(126, 202, 109, 14);
+			frame.getContentPane().add(idmaxCharacters);
+			
+			//afm
+			
+			JLabel afmLabel = new JLabel("AFM :");
+			afmLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+			afmLabel.setBounds(26, 230, 71, 14);
+			frame.getContentPane().add(afmLabel);
+			
+			JTextField afmTXT = new JTextField();
+			afmTXT.setColumns(10);
+			afmTXT.setBounds(126, 229, 257, 20);
+			frame.getContentPane().add(afmTXT);
+			
+			JLabel afmMaxCharacters = new JLabel("(10 characters)");
+			afmMaxCharacters.setFont(new Font("Tahoma", Font.PLAIN, 10));
+			afmMaxCharacters.setBounds(126, 245, 109, 14);
+			frame.getContentPane().add(afmMaxCharacters);
+			
+			//phone
+			
+			JLabel phonelabel = new JLabel("Phone :");
+			phonelabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+			phonelabel.setBounds(26, 278, 71, 14);
+			frame.getContentPane().add(phonelabel);
+			
+			JTextField phoneTXT = new JTextField();
+			phoneTXT.setColumns(10);
+			phoneTXT.setBounds(126, 277, 257, 20);
+			frame.getContentPane().add(phoneTXT);
+			
+			JLabel phoneMaxCharacters = new JLabel("(10 characters)");
+			phoneMaxCharacters.setFont(new Font("Tahoma", Font.PLAIN, 10));
+			phoneMaxCharacters.setBounds(126, 296, 109, 14);
+			frame.getContentPane().add(phoneMaxCharacters);
+			
+			//add button
+			
+			JButton addButton = new JButton("ADD");
+			addButton.setFont(new Font("Tahoma",Font.BOLD, 15));
+			addButton.setBounds(425, 340, 89, 23);
+			frame.getContentPane().add(addButton);
+			
+			addButton.addActionListener(new ActionListener() 
+			{
+				public void actionPerformed(ActionEvent e) 
+				{
+					
+					String name = nameTXT.getText();
+					String lastName = lastNameTXT.getText();
+					String id = idTXT.getText();
+					String afm = afmTXT.getText();
+					String phone = phoneTXT.getText();
+					
+				
+					errorFlag = checkError(name,lastName,id,afm,phone);
+					
+					if(!errorFlag)
+					{
+						frame.setVisible(false);
+						Buyer buyer = new Buyer(name,lastName,id,afm,phone);
+						seller.addBuyer(buyer);
+						
+					}
+					
+					
+				}
+			});
+			
+			
+			
+			frame.setSize(540, 413); 
+			frame.setTitle("Add A Client");
+			frame.setLayout(null); 
+			frame.setVisible(true);
+		}
+		
+		private boolean checkError(String name,String lastName,String id,String afm,String phone)
+		{
+			//checks if all fields are filled
+			
+			errorFlag = false;
+			
+			if(name.isEmpty())
+			{
+				JOptionPane.showMessageDialog(null, "Name Field Must Be Filled", "Invalid Input",JOptionPane.ERROR_MESSAGE);
+				errorFlag = true;
+			}	
+			
+			if(lastName.isEmpty())
+			{
+				JOptionPane.showMessageDialog(null, "Last Name Field Must Be Filled", "Invalid Input",JOptionPane.ERROR_MESSAGE);
+				errorFlag = true;
+			}
+			
+			if(id.isEmpty())
+			{
+				JOptionPane.showMessageDialog(null, "Buyer ID Field Must Be Filled", "Invalid Input",JOptionPane.ERROR_MESSAGE);
+				errorFlag = true;
+			}
+			if(!id.isEmpty())
+			{
+				if(id.length() != 6)
+				{
+					JOptionPane.showMessageDialog(null,"Wrong ID Input", "Invalid Input",JOptionPane.ERROR_MESSAGE);
+				}
+			}
+			
+			if(afm.isEmpty())
+			{
+				JOptionPane.showMessageDialog(null, "AFM Field Must Be Filled","Invalid Input",JOptionPane.ERROR_MESSAGE);
+				errorFlag = true;
+			}
+			if(!afm.isEmpty())
+			{
+				if(afm.length() != 10)
+				{
+					JOptionPane.showMessageDialog(null, "Wrong AFM Input","Invalid Input",JOptionPane.ERROR_MESSAGE);
+				}
+			}
+			
+			if(phone.isEmpty())
+			{
+				JOptionPane.showMessageDialog(null, "Phone Field Must Be Filled","Invalid Input",JOptionPane.ERROR_MESSAGE);
+				errorFlag = true;
+			}
+			if(!phone.isEmpty())
+			{
+				if(phone.length() != 10)
+				{
+					JOptionPane.showMessageDialog(null, "Wrong Phone Input","Invalid Input",JOptionPane.ERROR_MESSAGE);
+					errorFlag = true;
+				}
+			}
+			
+			//checks if buyer already exists in database
+			
+			if((!id.isEmpty()) && (id.length() == 6 ))
+			{
+				Buyers buyer = new Buyers();
+				ArrayList<Buyer> buyerID = buyer.getBuyers(); 
+				for(int i = 0; i < buyerID.size(); i++)
+				{
+					if(buyerID.get(i).getId().equals(id))
+					{
+						JOptionPane.showMessageDialog(null, "Client with the same ID already exists","Invalid Input",JOptionPane.ERROR_MESSAGE);
+						errorFlag = true;
+					}
+				}
+			}
+			
+			return errorFlag;
+		}
+		
+	}
+
--- src/SelectionGui.java
+++ src/SelectionGui.java
@@ -1,4 +1,514 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class SelectionGui {
+import javax.swing.*;
 
+public class SelectionGui extends JFrame
+{
+	private String[] options;
+	 //options[0] variable
+	 //options[1] filter
+       //options[2] key
+    //options[3] chart
+    //options[4] from month
+	 //options[5] from year
+	 //options[6] to month
+	 //options[7] to year
+	 
+	private Boolean errorFlag; //false if there is no error else true
+	private OrderManager om;
+	private Seller seller;
+	private String user;
+	private String[] months;
+	private JPanel panel;
+	private int user2; //0 if ordermanager, 1 if seller created to help method checkerror for options[3] input
+	
+	public SelectionGui(OrderManager om)
+	{	
+		user = "om";
+		errorFlag = false;
+		options = new String[8];
+		months = new String [12];
+		months[0] = "January";
+		months[1] = "February";
+		months[2] = "March";
+		months[3] = "April";
+		months[4] = "May";
+		months[5] = "June";
+		months[6] = "July";
+		months[7] = "August";
+		months[8] = "September";
+		months[9] = "October";
+		months[10] = "November";
+		months[11] = "December";
+		
+		
+	}
+
+	public SelectionGui(Seller seller)
+	{	
+		user = "seller";
+		errorFlag = false;
+		options = new String[8];
+		months = new String [12];
+		months[0] = "January";
+		months[1] = "February";
+		months[2] = "March";
+		months[3] = "April";
+		months[4] = "May";
+		months[5] = "June";
+		months[6] = "July";
+		months[7] = "August";
+		months[8] = "September";
+		months[9] = "October";
+		months[10] = "November";
+		months[11] = "December";
+		
+		
+	}
+	
+
+	public String[] getOptions()
+	{
+		return options;
+	}
+	
+	public String[] getMonths()
+	{
+		return months;  
+	}
+	
+	public void initialize() 
+	{
+		
+		JFrame f = new JFrame();
+		JPanel panel=new JPanel();  
+		
+		f.getContentPane().setBackground(new Color(176, 224, 230));
+
+		if(user.equals("om"))
+		{
+			user2 = 0;
+			String OMvariables[] = {"","Cost","Orders"};
+			String OMfilter[] = {"","Date","Supplier","Product"};
+			String charts[] = {"","Pie Chart","Line Chart","Bar Chart","Matrix"};
+					
+			JLabel var = new JLabel("Choose Variable :");
+			var.setBounds(8, 20, 100, 14);
+			f.getContentPane().add(var);
+			
+			JComboBox varCb = new JComboBox(OMvariables);   
+			varCb.setBounds(117, 16, 89, 22);    
+		    	f.getContentPane().add(varCb);  
+		    
+		    	JLabel filter = new JLabel("Choose Filter :");
+		    	filter.setBounds(8, 66, 100, 14);
+			f.getContentPane().add(filter);
+			
+			JComboBox filterCb = new JComboBox(OMfilter);
+			filterCb.setBounds(117, 62, 89, 22);
+			f.getContentPane().add(filterCb);
+			
+			JLabel key = new JLabel("Enter Key :");
+			key.setBounds(8, 113, 100, 14);
+			f.getContentPane().add(key);
+			
+			JTextField Ekey = new JTextField();
+			Ekey.setBounds(117, 110, 89, 20);
+			f.getContentPane().add(Ekey);
+			
+			JLabel message = new JLabel("Enter Key only if chosen filter is product or supplier");
+			message.setBounds(8, 138,300, 14);
+			f.getContentPane().add(message);
+			
+			JLabel chart = new JLabel("Choose Chart : ");
+			chart.setBounds(8, 182, 100, 14);
+			f.getContentPane().add(chart);
+			
+			JComboBox chartCb = new JComboBox(charts);
+			chartCb.setBounds(117, 178, 89, 22);
+			f.getContentPane().add(chartCb);
+			
+		    	JLabel from = new JLabel("From :");
+			from.setBounds(8, 226, 46, 14);
+			f.getContentPane().add(from);
+			
+			JComboBox fromMonth = new JComboBox(months);
+			fromMonth.setBounds(117, 229, 89, 22);
+			f.getContentPane().add(fromMonth);
+			
+			JLabel to = new JLabel("To :");
+			to.setBounds(8, 273, 46, 14);
+			f.getContentPane().add(to);
+			
+			JComboBox toMonth = new JComboBox(months);
+			toMonth.setBounds(117, 269, 89, 22);
+			f.getContentPane().add(toMonth);
+			
+			JLabel enterYear = new JLabel("Enter Year :");
+			enterYear.setBounds(228, 214, 75, 14);
+			f.getContentPane().add(enterYear);
+			
+			JTextField yearFrom = new JTextField();
+			yearFrom.setBounds(228, 231, 58, 20);
+			f.getContentPane().add(yearFrom);
+			yearFrom.setColumns(10);
+			
+			JTextField yearTo = new JTextField();
+			yearTo.setColumns(10);
+			yearTo.setBounds(228, 270, 58, 20);
+			f.getContentPane().add(yearTo);
+			
+			JButton b = new JButton("Done");
+			b.setBounds(415, 310, 71, 23); 
+			f.getContentPane().add(b);
+			
+			
+			b.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent e) 
+				{
+					options[0] = (String) varCb.getSelectedItem();
+					
+					options[1] =(String) filterCb.getSelectedItem();
+					
+					options[2] = Ekey.getText();
+					
+					options[3] = (String) chartCb.getSelectedItem();
+					
+					options[4] = (String) fromMonth.getSelectedItem();
+					
+					options[5] = yearFrom.getText();
+					
+					options[6] = (String) toMonth.getSelectedItem();
+					
+					options[7] = yearTo.getText();
+					
+					errorFlag = checkError(options,user2);
+
+					if(!errorFlag)
+					{
+						f.setVisible(false);
+						Statistics ord = new Statistics(options,months);
+						ord.OrdererStat(om);
+	
+					}
+				
+					}
+					
+			});
+			
+			
+			f.setSize(512, 383); 
+			f.setTitle("Order Mnager Statistics");
+			f.setLayout(null); 
+			f.setVisible(true);
+		
+		}
+		else if(user.equals("seller"))
+		{
+			user2 = 1;
+			String OMvariables[] = {"","Profit","Purchases"};
+			String OMfilter[] = {"","Date","Client","Product"};
+			String charts[] = {"","Pie Chart","Line Chart","Bar Chart","Matrix"};
+					
+			JLabel var = new JLabel("Choose Variable :");
+			var.setBounds(8, 20, 100, 14);
+			f.getContentPane().add(var);
+			
+			JComboBox varCb = new JComboBox(OMvariables);   
+			varCb.setBounds(117, 16, 89, 22);    
+		    f.getContentPane().add(varCb);  
+		    
+		    JLabel filter = new JLabel("Choose Filter :");
+		    filter.setBounds(8, 66, 100, 14);
+			f.getContentPane().add(filter);
+			
+			JComboBox filterCb = new JComboBox(OMfilter);
+			filterCb.setBounds(117, 62, 89, 22);
+			f.getContentPane().add(filterCb);
+			
+			JLabel key = new JLabel("Enter Key :");
+			key.setBounds(8, 113, 100, 14);
+			f.getContentPane().add(key);
+			
+			JTextField Ekey = new JTextField();
+			Ekey.setBounds(117, 110, 89, 20);
+			f.getContentPane().add(Ekey);
+			
+			JLabel message = new JLabel("Enter ID only if chosen filter is Product or Client");
+			message.setBounds(8, 138,300, 14);
+			f.getContentPane().add(message);
+			
+			JLabel chart = new JLabel("Choose Chart : ");
+			chart.setBounds(8, 182, 100, 14);
+			f.getContentPane().add(chart);
+			
+			JComboBox chartCb = new JComboBox(charts);
+			chartCb.setBounds(117, 178, 89, 22);
+			f.getContentPane().add(chartCb);
+			
+		    JLabel from = new JLabel("From :");
+			from.setBounds(8, 226, 46, 14);
+			f.getContentPane().add(from);
+			
+			JComboBox fromMonth = new JComboBox(months);
+			fromMonth.setBounds(117, 229, 89, 22);
+			f.getContentPane().add(fromMonth);
+			
+			JLabel to = new JLabel("To :");
+			to.setBounds(8, 273, 46, 14);
+			f.getContentPane().add(to);
+			
+			JComboBox toMonth = new JComboBox(months);
+			toMonth.setBounds(117, 269, 89, 22);
+			f.getContentPane().add(toMonth);
+			
+			JLabel enterYear = new JLabel("Enter Year :");
+			enterYear.setBounds(228, 214, 75, 14);
+			f.getContentPane().add(enterYear);
+			
+			JTextField yearFrom = new JTextField();
+			yearFrom.setBounds(228, 231, 58, 20);
+			f.getContentPane().add(yearFrom);
+			yearFrom.setColumns(10);
+			
+			JTextField yearTo = new JTextField();
+			yearTo.setColumns(10);
+			yearTo.setBounds(228, 270, 58, 20);
+			f.getContentPane().add(yearTo);
+			
+			JButton b = new JButton("Done");
+			b.setBounds(415, 310, 71, 23); 
+			f.getContentPane().add(b);
+			
+			
+			b.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent e) 
+				{
+					options[0] = (String) varCb.getSelectedItem();
+					
+					options[1] =(String) filterCb.getSelectedItem();
+					
+					options[2] = Ekey.getText();
+					
+					options[3] = (String) chartCb.getSelectedItem();
+					
+					options[4] = (String) fromMonth.getSelectedItem();
+					
+					options[5] = yearFrom.getText();
+					
+					options[6] = (String) toMonth.getSelectedItem();
+					
+					options[7] = yearTo.getText();
+					
+					errorFlag = checkError(options, user2);
+					
+					if(!errorFlag)
+					{
+						f.setVisible(false);
+						Statistics sup = new Statistics(options, months);
+						sup.SupplierStat(seller);
+						
+					}
+					
+				}
+			
+			});
+		
+			
+			
+			f.setSize(512, 383); 
+			f.setTitle("Supplier Statistics");
+			f.setLayout(null); 
+			f.setVisible(true);
+		
+		}
+	}
+		private boolean checkError(String[] options, int user2)
+		{
+			int fromMonth = 0;
+			int toMonth = 0;
+			boolean keyExists = false; //true if key(id) of options[3] input exists in database
+			
+			errorFlag = false;
+			
+			//checking variable input
+			
+			if(options[0].isEmpty())
+			{
+				errorFlag = true;
+				
+				JOptionPane.showMessageDialog(null, "Variable Field Must Be Filled", "Invalid Input",JOptionPane.ERROR_MESSAGE);
+			}
+			
+			//checking filter input
+			
+			if(options[1].isEmpty())
+			{
+				errorFlag = true;
+				
+				JOptionPane.showMessageDialog(null, "Filter Field Must Be Filled", "Invalid Input",JOptionPane.ERROR_MESSAGE);
+			}
+			
+			//checking key input
+			
+			if((options[1] == "Supplier") || (options[1] == "Product") || (options[1] == "Client"))
+			{
+				if(options[2].isEmpty())
+				{
+					errorFlag = true;
+					JOptionPane.showMessageDialog(null, "Key Field Must Be Filled", "Invalid Input",JOptionPane.ERROR_MESSAGE);
+					
+				}
+				
+			}	
+			  
+		    //checking if options[2](key) input exists in database
+		     
+		     if((!options[2].isEmpty()) && (user2 == 0) )
+		     {
+		    	 if(options[2].equals("Product"))
+		    	 {
+			    	SupplierProducts productS = new SupplierProducts();
+			    	ArrayList<SupplierProduct> sProd = productS.getSupplierProducts();
+			    	for(int i = 0; i < sProd.size(); i++)
+			    	{
+			    		if(options[2].equals(sProd.get(i).getId()))
+			    		{
+			    			keyExists = true;
+			    		}
+			    	}
+		    	 }
+		    	 else if (options[2].equals("Supplier"))
+		    	 {
+		    		 Suppliers sup = new Suppliers();
+		    		 ArrayList <Supplier> supplier = sup.getSuppliers();
+		    		 for(int i = 0; i < supplier.size(); i++)
+		    		 {
+		    			 if(options[2].equals(supplier.get(i).getId()))
+		    			 {
+		    				 keyExists = true;
+		    			 }
+		    		 }
+		    	 }
+		     }
+		     
+		     if((keyExists == false) && (user2 == 0))
+		     {
+		    	 errorFlag = true;
+		    	 JOptionPane.showMessageDialog(null, "Key ID doesn't exist.", "Invalid Input",JOptionPane.ERROR_MESSAGE);		
+		     }
+		     
+		     if((!options[2].isEmpty()) && (user2 == 1) )
+		     {
+		    	 if(options[2].equals("Product"))
+		    	 {
+			    	CompanyProducts prod = new CompanyProducts();
+			    	ArrayList<CompanyProduct> cProd = prod.getCompProduct();
+			    	for(int i = 0; i < cProd.size(); i++)
+			    	{
+			    		if(options[2].equals(cProd.get(i).getId()))
+			    		{
+			    			keyExists = true;
+			    		}
+			    	}
+		    	 }
+		    	 else if (options[2].equals("Client"))
+		    	 {
+		    		 Buyers b = new Buyers();
+		    		 ArrayList <Buyer> buyer = b.getBuyers();
+		    		 for(int i = 0; i < buyer.size(); i++)
+		    		 {
+		    			 if(options[2].equals(buyer.get(i).getId()))
+		    			 {
+		    				 keyExists = true;
+		    			 }
+		    		 }
+		    	 }
+		     }
+		     
+		     if((keyExists == false) && (user2 == 1))
+		     {
+		    	 errorFlag = true;
+		    	 JOptionPane.showMessageDialog(null, "Key doesn't exist.", "Invalid Input",JOptionPane.ERROR_MESSAGE);		
+		     }
+		     
+		     //checking chart input
+				
+		     if(options[3].isEmpty())
+			{
+				errorFlag = true;
+				
+				JOptionPane.showMessageDialog(null, "Chart Field Must Be Filled", "Invalid Input",JOptionPane.ERROR_MESSAGE);		
+			}
+		     
+		     //checking date input
+		     
+		     if(options[5].isEmpty())
+		     {
+		    	 JOptionPane.showMessageDialog(null, "Enter Year From Field Must Be Filled", "Invalid Input",JOptionPane.ERROR_MESSAGE);
+		     }
+		     if(options[7].isEmpty())
+		     {
+		    	 JOptionPane.showMessageDialog(null, "Enter Year To Field Must Be Filled", "Invalid Input",JOptionPane.ERROR_MESSAGE);
+		     }
+		     if(options[5].compareTo(options[7]) > 0)
+		     {
+		    	 JOptionPane.showMessageDialog(null, "Wrong Year Input.\"From Year\" Later Than \"To Year\"", "Invalid Input",JOptionPane.ERROR_MESSAGE);
+		     }
+		     if(options[5].compareTo(options[7])== 0)
+			{
+				for(int i = 0; i< 12; i++)
+				{
+					if(options[4].equals(months[i]))
+					{
+						fromMonth = i;
+					}
+					if(options[6].equals(months[i]))
+					{
+						toMonth = i;
+					}
+				}
+				
+				if(fromMonth > toMonth)
+				{
+					errorFlag = true;
+					JOptionPane.showMessageDialog(null,"Wrong Year Input.\"From Month\" Later Than \"To Month\"", "Invalid Input",JOptionPane.ERROR_MESSAGE);
+				}
+				else if((fromMonth == toMonth) || ((toMonth - fromMonth) < 2))
+				{
+					errorFlag = true;
+					JOptionPane.showMessageDialog(null,"For the same year,month period must be at least two months apart", "Invalid Input",JOptionPane.ERROR_MESSAGE);
+				}
+			}
+			
+		     //checking if year input is correct
+		     
+			if(!options[5].isEmpty())
+			{
+				if(((Integer.parseInt(options[5])) < 2010) || (Integer.parseInt(options[5])) > 2040)
+				{
+					errorFlag = true;
+					JOptionPane.showMessageDialog(null,"Wrong \"From Year\"  input", "Invalid Input",JOptionPane.ERROR_MESSAGE);
+				}
+			}
+			
+			if(!options[7].isEmpty())
+			{
+				if(((Integer.parseInt(options[7])) < 2010) || (Integer.parseInt(options[7])) > 2040)
+				{
+					errorFlag = true;
+					JOptionPane.showMessageDialog(null,"Wrong \"To Year\"  input", "Invalid Input",JOptionPane.ERROR_MESSAGE);
+				}
+			}
+			return errorFlag;
+		}
+		
 }
+
+		
