d41ba39b003994e8afb3af049ee76de837365f33
EvangeliaaP <58250802+EvangeliaaP@users.noreply.github.com>
2020-06-01 17:31:29 +0300

    Update Users.java
    Comment 2.0

--- src/Users.java
+++ src/Users.java
@@ -1,19 +1,26 @@
 import java.sql.*;
 import java.util.ArrayList;
 
+/**
+ * Users.java
+ * Inherits from ListFromDB.java
+ * Purpose: Manages the data from table User (in simplify.db).
+ * @author Eleni Polyzoidou, Evangelia Papagiannaki.
+ */
 public class Users extends ListFromDB {
 	private ArrayList<User> users = new ArrayList<User>();
 
-	private Statement statement = null;		//statement for users that have type OrderManager
+	private Statement statement = null;//statement for users that have type OrderManager
+	
 	/**
-	 *
+	 * Loads data in users.
 	 */
-
+	@Override
 	public void extractObjectDB() {
 		Connection c = connect();
 		try {
 			statement = c.createStatement();
-			ResultSet rs = statement.executeQuery("SELECT * FROM User  WHERE id NOT IN (SELECT id FROM OrderManager)");
+			ResultSet rs = statement.executeQuery("SELECT * FROM User  WHERE id NOT IN (SELECT id FROM OrderManager)");  
 			while (rs.next()) {
 				
 				User us = new User();
@@ -26,8 +33,7 @@ public class Users extends ListFromDB {
 				us.setCompany(rs.getString("Company"));
 				users.add(us);
 			}
-			for(User user: users)
-			{
+			for(User user: users){
 				int index = users.indexOf(user);
 				if (user.getId().startsWith("ST")) {
 					Stockkeeper st = new Stockkeeper(user.getFirstName(),user.getSurName(),user.getPassword(),user.getPhonenumber(),user.getAFM(),user.getId(),user.getCompany());
@@ -74,9 +80,11 @@ public class Users extends ListFromDB {
 			}
 		}
 	}
-	
-	public ArrayList<User> getUsers()
-	{
+	/**
+	 * Gets the list with objects of type User.
+	 * @return users   List that contain object of type User
+	 */
+	public ArrayList<User> getUsers(){
 		return users;
 	}
 }
