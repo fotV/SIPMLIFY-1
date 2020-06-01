a3f1561f2a7715e8817b6cd3f0bb2a4bb6655393
EvangeliaaP <58250802+EvangeliaaP@users.noreply.github.com>
2020-06-01 17:13:12 +0300

    Update CompanyProducts.java
    Comment 2.0

--- src/CompanyProducts.java
+++ src/CompanyProducts.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 /**
  * CompanyProducts.java
  * Inherits from LIstFromDB.java
- * Purpose: Manages the data from matrix Product_for_sale (in simplify.db).
+ * Purpose: Manages the data from table Product_for_sale (in simplify.db).
  * @author Eleni Polyzoidou, Evangelia Papagiannaki.
  */
 public class CompanyProducts extends ListFromDB {
