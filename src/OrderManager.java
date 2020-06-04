47e5f747aee0777ebdedf029f2bd3654872d4d96
papagianEva <evapapag2000@gmail.com>
2020-06-04 20:39:48 +0300

    no message

--- src/OrderManager.java
+++ src/OrderManager.java
@@ -1,5 +1,6 @@
 import java.awt.Component;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.swing.JOptionPane;
 
@@ -8,7 +9,6 @@ public class OrderManager extends User {
 	private boolean regular = false ;
 	private String season ;
 	private SupplierProducts products ;
-	private Orders orders ;
 	private Proposals proposals ;
 	private Suppliers suppliers ;
 	private Supplies supplies ;
@@ -18,35 +18,74 @@ public class OrderManager extends User {
 		super(firstName, surName, password, telephone, AMA, company, id);
 		this.regular = regular;
 		this.season = season;
+		products = new SupplierProducts();
 		proposals = new Proposals();
 		suppliers = new Suppliers();
 		supplies = new Supplies();
+		
 	}
 	
 	/* Method initializeLists() : extracts the infomations from database 
 	** and adds them into lists */
 	public void initializeLists() {
 		
-		/* Extracts the products from DB of the specific order manager */
-		for (SupplierProduct supProd : products.getSupplierProducts()) {
-			if ( supProd.getOrderManagerId().equals(id) ) {
-				products.extractObjectDB();
+		this.orders.extractObjectDB();
+		ArrayList<Order> ord = new ArrayList<Order>();
+		for (Order o: this.orders.getOrders()) {
+			if(o.getOrderManagerId().equals(this.id))
+				ord.add(o);
+		}
+		this.orders.getOrders().clear();
+		this.orders.getOrders().addAll(ord);
+		   
+		this.products.extractObjectDB();                           // Extracts the products from DB for the specific order manager      
+		ArrayList<SupplierProduct> supProducts = new ArrayList<SupplierProduct>();
+		for (SupplierProduct supProd : this.products.getSupplierProducts()) {
+			if ( supProd.getOrderManagerId().equals(this.id) ) {
+				supProducts.add(supProd);
 			}
 		}
+		this.products.getSupplierProducts().clear();
+		this.products.getSupplierProducts().addAll(supProducts);
 		
-		/* Extracts the suppliers from DB of the specific order manager */
-		for (Supplier suppl : suppliers.getSuppliers()) {
-			if ( suppl.getOrderManagerId().equals(id) ) {
-				suppliers.extractObjectDB();
+		this.suppliers.extractObjectDB();							//Extracts the suppliers for DB of the specific order manager 
+		ArrayList<Supplier> supp = new ArrayList<Supplier>();
+		for (Supplier suppl : this.suppliers.getSuppliers()) {
+			if ( suppl.getOrderManagerId().equals(this.id) ) {
+				supp.add(suppl);
 			}
 		}
+		this.suppliers.getSuppliers().clear();
+		this.suppliers.getSuppliers().addAll(supp);
+		 
+		this.proposals.extractObjectDB();
+		ArrayList<Order> prop = new ArrayList<Order>();
+		for (Order order: proposals.getProposals()) {
+			if ( order.getOrderManagerId().equals(this.id)){
+				prop.add(order);
+			}
+		}
+		this.proposals.getProposals().clear();
+		this.proposals.getProposals().addAll(prop);
 		
-		proposals.extractObjectDB();
 		supplies.extractObjectDB();
+		ArrayList<Suppl> supll = new ArrayList<Suppl>();
+		for (Suppl s : supplies.getSupplies()) {
+			for (Supplier sp: this.suppliers.getSuppliers()) {
+				if ( s.getSupplierId().equals(sp.getId())) 
+					supll.add(s);	
+			}
+		}
+		this.supplies.getSupplies().clear();
+		this.supplies.getSupplies().addAll(supll);
+		
+		
 	}
 	
-	/* Method searchForProduct(): searches a product and calls a GUI to 
-	** to show the results */
+	/* Method searchForProduct(): searches a product and calls a GUI to  to show the results 
+	 * @Param key     Represents the key for searching
+	 * @Param column  Represents the
+	**/
 	public void searchForProduct(String key, int column) {
 		Boolean found=false;
 		ArrayList<Object> productsKEY = new ArrayList<>();
