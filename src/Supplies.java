d2274c99b3c2eaadbbc5333a3047791eeb6439a5
EvangeliaaP <58250802+EvangeliaaP@users.noreply.github.com>
2020-06-11 22:37:00 +0300

    Update Supplies.java

--- src/Supplies.java
+++ src/Supplies.java
@@ -1,14 +1,19 @@
 
 import java.sql.*;
 import java.util.ArrayList;
-
+/**
+* Supplies.java
+* Inherits from ListFromDB.java
+* Purpose: Extracts data and updates database for Supplies.
+* @author Evangelia Papagiannaki, Eleni Polyzoidou.
+*/
 public class Supplies extends ListFromDB {
 	ArrayList<Suppl> supplies = new ArrayList<>();
-	
+	/**
+	 * Extracts data, creates new Suppl and adds them to list supplies
+	 */
+	@Override
 	public void extractObjectDB() {
-		/*
-		 * 
-		 */
 		Connection c = connect();
 		try {
 			
@@ -34,15 +39,13 @@ public class Supplies extends ListFromDB {
 			}
 		}
 	}
-	
-	public void updateObjectDB()
-	{
-		/*
-		 * 
-		 */
+	/**
+ 	 * Updates old records and inserts new ones into arrays Supplies
+	 */
+	@Override
+	public void updateObjectDB(){
 		Connection c = connect();
 		try {
-			
 			String insertIntoSupplies = "INSERT OR IGNORE INTO Supplies (Supplier_Id, PFP_Id, Price) VALUES (?,?,?);";
 			PreparedStatement statementSupplies = c.prepareStatement(insertIntoSupplies);
 			for(Suppl s: supplies) {
@@ -63,8 +66,12 @@ public class Supplies extends ListFromDB {
 			}
 		}
 	}
-	public ArrayList<Suppl> getSupplies()
-	{
+	
+	/**
+	 * Gets the list of Suppl
+	 * @return An ArrayList with Suppl
+	 */
+	public ArrayList<Suppl> getSupplies(){
 		return supplies;
 	}
  
