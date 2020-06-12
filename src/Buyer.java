6033fe87b306d28991fafa956854c04530b6413b
fotV <64465017+fotV@users.noreply.github.com>
2020-06-12 12:04:14 +0300

    Update Buyer.java

--- src/Buyer.java
+++ src/Buyer.java
@@ -1,70 +1,7 @@
 
 
 public class Buyer {
-	/*Buyer.java
-	 * @author Vlasakoudi Christina
-	 */
-	private String name;
-	private String lastName;
-	private String id;
-	private String AFM;
-	private String phoneNumber;
-	private String sellerId;
 	
-
-	public Buyer(String name,String lastName,String id,String AFM,String phoneNumber,String sellerId) {
-	
-		this.name = name;
-		this.lastName = lastName;
-		this.id = id;
-		this.AFM = AFM;
-		this.phoneNumber = phoneNumber;
-		this.sellerId = sellerId;
-}
-
-	public String getName() {
-		return name;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public String getAFM() {
-		return AFM;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public String getSellerId() {
-		return sellerId;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;	
-	}
-	public void setId(String id) {
-		this.id = id;	
-	}
-	public void setAFM(String afm) {
-		this.AFM = afm;	
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
-	public void setSellerId(String sellerId) {
-		this.sellerId = sellerId;
-		
-	}
-
 	
 }
 
